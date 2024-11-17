39e9ca8dc837753047c910ccb3f138e60b10ae13
Vibin Sagar <vibinveliyath@gmail.com>
2024-11-17 14:58:16 +0530

    latest

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -4,7 +4,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import MyNavbar from "../mainHeader";
 import ChangeProfilePic from "../../helpers/ChangeProfilePic";
 import Context from "../../context/context";
-import ProfileDetials from "./ProfileDetials";
+import ProfileDetials from "./";
 import UpdateProfilePage from "../UpdateProfilePage";
 
 export default function ProfilePage() {
